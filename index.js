b06f8ab9a5d57e23caa49420d90a1d7fd2bd4978
darshan-at <43428382+darshan-at@users.noreply.github.com>
2021-07-21 18:47:05 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-var TxtType = function(el, toRotate, period) {
+var TxtType = function (el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
@@ -9,44 +8,46 @@ var TxtType = function(el, toRotate, period) {
     this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function() {
+TxtType.prototype.tick = function () {
     var i = this.loopNum % this.toRotate.length;
     var fullTxt = this.toRotate[i];
 
     if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
     } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
     var that = this;
     var delta = 200 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
+    if (this.isDeleting) {
+        delta /= 2;
     }
 
-    setTimeout(function() {
-    that.tick();
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
     }, delta);
 };
 
-window.onload = function() {
+window.onload = function () {
     var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
+    for (var i = 0; i < elements.length; i++) {
         var toRotate = elements[i].getAttribute('data-type');
         var period = elements[i].getAttribute('data-period');
         if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
+            new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
     // INJECT CSS
@@ -56,15 +57,15 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
-/*Media Queury for title Margin in HOME PAGE*/
-function reduceMargin(x) {
-    if (x.matches) { // If media query matches
-      var check=document.getElementsByClassName("show");
-      console.log(check[0]);
-
-    }
-  }
-var med=window.matchMedia("(max-width:850px)");
-reduceMargin(med);
-
-med.addEventListener("change",reduceMargin);
+/*Navigation Bar Active Button*/
+var logo = $(".navbar-brand");
+$(".navbar-brand").mouseover(function () {
+    logo[0].src = "images/laptop-memoji.png";
+}).mouseout(function () {
+    logo[0].src = "images/memoji.png";
+});
+/*$(".project-card").mouseover(function(){
+    var projectObject = $(this);
+    var projectImage=projectObject[0].firstElementChild;
+    projectImage.style.display="none";
+});*/
